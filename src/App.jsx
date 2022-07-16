3fe147732fac7b1f9f1882de33a506ecf4419e99
Haris <hariskumar.eth@gmail.com>
2022-07-16 05:46:53 +0100

    commit by Haris

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Main from './comp/Main'
+
+function App() {
+  // document.addEventListener('contextmenu', event => event.preventDefault());
+
+  return (
+   <div className='overflow-hidden'>
+      <Main />
+      
+   </div>
+  )
+}
+
+export default App
---
+++ src/comp/Main.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div>
+      <section className="min-h-screen w-full bg-blue-500 font-2p text-white flex flex-col justify-center items-end overflow-hidden fixed ">
+
+        <h1 className="logo text-2xl md:text-3xl absolute left-6 md:left-10 top-5 md:top-10 font-monument font-bold tracking-wider select-none">HK</h1>
+
+            <h1 className='text-2xl px-4 md:px-0 md:text-4xl md:w-3/4 md:leading-normal font-bold tracking-tight absolute top-1/3 left-1/2 -translate-x-1/2 text-center z-50 w-full leading-loose'>This Website is Under Development :) 
+            <br /><a href="https://www.twitter.com/Haris_ETH"><span className="text-lg md:text-xl animate-pulse">by Haris</span></a>
+            </h1>
+
+            <img src="http://www.videogamesprites.net/SuperMarioBros1/Scenery/Bush%20-%20Triple.gif" alt="BUSH" className='absolute  w-1/3 md:w-52 bottom-16 left-0' />
+
+            <img src="http://www.videogamesprites.net/SuperMarioBros1/Scenery/Bush%20-%20Single.gif" alt="" className="absolute w-24 md:w-28 bottom-16 right-10" />
+    
+
+            <div className="ground absolute bottom-0 h-16 flex overflow-hidden z-20">
+              
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' /><img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' /><img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' /><img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Items/Bricks.gif" alt="Mario Ground Block" className='h-full' />
+            </div>
+
+            <div className="clouds flex flex-col gap-4 overflow-hidden z-30">
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Scenery/Cloud%20-%20Double.gif" alt="Clouds GIF" className='cloudAnimOne absolute top-1/2 h-16' />
+
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Scenery/Cloud%20-%20Double.gif" alt="Clouds GIF" className='cloudAnimTwo w-28 absolute top-28 md:w-40'
+                />
+
+              <img src="http://www.videogamesprites.net/SuperMarioBros1/Scenery/Cloud%20-%20Double.gif" alt="Clouds GIF" className='cloudAnimThree w-36 top-2/3 absolute' />
+            </div>
+
+            <div className="mario absolute bg-no-repeat bottom-16 z-50"></div>
+      </section>
+    </div>
+  )
+}
